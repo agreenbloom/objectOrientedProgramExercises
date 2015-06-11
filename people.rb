41492916962cb862e6904886c0ac0d4495030e98
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-11 14:22:03 -0400

    initalize name mmethod for student and instructor

--- people.rb
+++ people.rb
@@ -1,21 +1,42 @@
-class person
-attr_accessor :name
+class Person
+  attr_accessor :name
 
-  class Students
-    def learn
-      "I get it!"
-    end
+  def initialize(name)
+    @name = name
   end
 
-  class Instructor
-    def teach
-      "Everything in Ruby is an Object"
-    end
+  def add(name)
+    Name.new(@name)
   end
 
-def to_s
-  "Hi, my name is #{name}"
-end
+  def display
+    "Hi, my name is #{name}"
+  end
 
 end
 
+class Student < Person
+  def initalize(name)
+    super(name)
+  end
+  def learn
+    "I get it!"
+  end
+end
+
+class Instructor < Person
+  def initalize(name)
+    super(name)
+  end
+
+  def teach
+   puts "Everything in Ruby is an Object"
+  end
+end
+
+
+instructor1 = Instructor.new("Chris")
+student1 = Student.new("Cristina")
+
+puts instructor1.teach
+puts student1.learn
