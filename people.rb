64f3d3c1cf70aa7106bf6fb880b9928c4777bb57
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-11 13:51:52 -0400

    creating methods for students and teachers names in a parent class called person

---
+++ people.rb
@@ -0,0 +1,21 @@
+class person
+attr_accessor :name
+
+  class Students
+    def learn
+      "I get it!"
+    end
+  end
+
+  class Instructor
+    def teach
+      "Everything in Ruby is an Object"
+    end
+  end
+
+def to_s
+  "Hi, my name is #{name}"
+end
+
+end
+
